234b6cfc8ae4d99954b71cbfda5bff54b6be6bec
May Comia <pmaycomia@gmail.com>
2021-06-14 23:25:03 +0200

    Create text and author components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
+import './App.css'
+
+
+
+const Text = () => {
+  return (
+    
+      <h2 id="text">
+        "If you can dream it, you can achieve it."
+      </h2>
+   
+  )
+}
+
+const Author = () => {
+  return(
+    <div>
+      <h3>Zig Ziglar</h3>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi!</h1>
+
+    <div className="container">
+      <h1>Random Quote Generator</h1>    
+
+      <div id="quote-box">
+        <Text />
+        <Author />
+        <button>new quote</button>
+      </div>
     </div>
   );
 }
