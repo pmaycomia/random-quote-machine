0f3fb717472460c8e627e331674000f4bba8c52f
May Comia <pmaycomia@gmail.com>
2021-06-15 23:19:43 +0200

    Get new quote upon clicking new-quote button

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
+import React, { useState } from 'react'
 import './App.css'
 import {FaTwitter} from "react-icons/fa"
 import QuotesList from './QuotesList.json'
 
-let randomizer = Math.floor(Math.random() * 10);
-let firstQuote = QuotesList[randomizer].quote
-let firstAuthor = QuotesList[randomizer].author
+
 
 const Text = (props) => {
-  return (
-    
+  return (    
       <h2 id="text">
         "{props.quote}"
-      </h2>
-   
+      </h2>   
   )
 }
 
@@ -24,27 +21,40 @@ const Author = (props) => {
   )
 }
 
-function App() {
-  return (
+function RandomQuotes() {
 
+  const [random, setRandom] = useState(0);
+  let randomizer;
+  let randQuote = QuotesList[random].quote
+  let randAuthor = QuotesList[random].author
+  
+  
+  const nextQuoteClick = () => {   
+    randomizer = () => Math.floor(Math.random() * QuotesList.length)
+    setRandom(randomizer)
+    console.log(random);
+    //how can I randomize without repeating previous number?
+  } 
+
+  return (
     <div className="container">
       <h1>Random Quote Generator</h1>    
 
       <div id="quote-box">
-        <Text quote={firstQuote} />
-        <Author author={firstAuthor} />
+        <Text quote={randQuote} />
+        <Author author={randAuthor} />
         <div className="bottom-spaceout"> 
-          <a id="tweet-quote"><FaTwitter/ > </a>
-          <button id="new-quote">new quote</button>  
-        </div>
-        
-        
+          <a href="https://www.twitter.com" id="tweet-quote"><FaTwitter/ > </a>
+          <button onClick={nextQuoteClick} id="new-quote">new quote</button>  
+        </div>        
       </div>
+
       <div className="footer">
         <h4>pmayc</h4>
       </div>
+      
     </div>
   );
 }
 
-export default App;
+export default RandomQuotes;
